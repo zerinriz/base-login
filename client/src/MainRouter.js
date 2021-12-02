6870d4f3d93639e516b1fd31534f5202534b4076
zerinriz <zerin_95@hotmail.com>
2021-12-02 18:27:16 +0100

    Base-login v2.0

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import PrivateRoute from "./user/PrivateRoute"
 import Profile from "./user/Profile";
 import Register from "./user/Register";
 import Login from "./auth/Login";
@@ -12,7 +13,7 @@ const MainRouter = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        <Route path="/user/:userId" component={Profile} />
+        <PrivateRoute path="/user/:userId" component={Profile} />
       </Switch>
     </>
   );
